6c0bef711a69ef42741e587588f4d358786eb99c
Priyanshu Ranjan <122288679+priyanshu-ranjan-00@users.noreply.github.com>
2025-04-09 21:22:18 +0530

    few changes in .env

--- Backend/config.js
+++ Backend/config.js
@@ -1,4 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const PORT = 5000;
 
-export const mongoDBURI =
-  "mongodb+srv://priyanshu323ranjan:priyanshu323ranjan@book-store.ake1ooa.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store";
+export const mongoDBURI = process.env.MONGODB_URI;
